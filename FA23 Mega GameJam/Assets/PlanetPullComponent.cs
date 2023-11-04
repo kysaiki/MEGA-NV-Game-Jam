ed636b14d738ae90234f4ca16de59a4499b1cb61
Kyler Saiki (he/him) <kylersaiki@gmail.com>
2023-11-04 13:12:30 -0700

    Added Gravity Pull Component
    Positive gravity attracts, negative repels

---
+++ FA23 Mega GameJam/Assets/PlanetPullComponent.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetPullComponent : MonoBehaviour
+{
+    [SerializeField] private float Gravity = -10.0f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<Rigidbody>().AddForce(CalculateGravitationForce(other.gameObject));
+        }
+    }
+
+    Vector3 CalculateGravitationForce(GameObject player)
+    {
+        GameObject gravityPlanet = this.transform.parent.gameObject;
+        float distance = Vector3.Distance(player.transform.position, gravityPlanet.transform.position);
+        float gravityStrength = Gravity / (distance * distance);
+        Vector3 playerToPlanetVector = gravityPlanet.transform.position - player.transform.position;
+        return playerToPlanetVector * gravityStrength;
+    }
+}
