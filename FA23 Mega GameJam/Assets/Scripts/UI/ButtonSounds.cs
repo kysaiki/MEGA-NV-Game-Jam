1ce6662e75fb46fa0eefaf0c12ccd6296a998f17
Emma Leihe <leihe@usc.edu>
2023-11-05 16:18:44 -0800

    main menu button ui sounds

---
+++ FA23 Mega GameJam/Assets/Scripts/UI/ButtonSounds.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ButtonSounds : MonoBehaviour
+{
+    public void PlaySound()
+    {
+        AudioManager.instance.PlaySFX(AudioManager.SoundEffect.UIButton);
+    }
+}
